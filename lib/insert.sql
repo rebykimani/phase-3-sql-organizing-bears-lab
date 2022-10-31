51232779c8ffd23d4131f29ef8bc9c1b31ac0726
rebykimani <beckireby15@gmail.com>
2022-10-31 17:16:33 +0300

    completed assignment

---
+++ lib/insert.sql
@@ -0,0 +1,64 @@
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES(
+    "MR. Chocolate",
+    19,
+    "M",
+    "brown",
+    "Melancholic",
+    1
+);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES(
+    "Grinch",
+    19,
+    "F",
+    "brown",
+    "Melancholic",
+    1
+);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES(
+    "Wendy",
+    19,
+    "M",
+    "brown",
+    "Melancholic",
+    1
+);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES(
+    "Tabitha",
+    19,
+    "M",
+    "brown",
+    "Melancholic",
+    1
+);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES(
+    "Sergeant Brown",
+    19,
+    "M",
+    "brown",
+    "Melancholic",
+    1
+);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES(
+    "Melissa",
+    19,
+    "M",
+    "brown",
+    "Melancholic",
+    1
+);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES(
+    "Rowdy",
+    10,
+    "M",
+    "brown",
+    "Melancholic",
+    1
+);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES(
+    NULL,
+    20,
+    "F",
+    "brown",
+    "Melancholic",
+    1
+);
